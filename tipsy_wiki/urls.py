315ff50ad4ba479a5ed807d7577ea6fb727d33c2
RyQcan <13723926068@163.com>
2019-05-15 22:28:26 +0800

    urls

--- tipsy_wiki/urls.py
+++ tipsy_wiki/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin, auth
-from wiki import views as news_views
+from wiki import views as wiki_views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     url(r'^$', wiki_views.index),
-    url(r'^news/', include('news.urls', namespace='news')),
+    url(r'^wiki/', include('wiki.urls', namespace='wiki')),
     # url('^', include('auth.urls', namespace='users')),
     url(r'^admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,48 @@
+# -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
 from django.db import models
 
-# Create your models here.
+
+class Column(models.Model):
+    name = models.CharField('栏目名称', max_length=256)
+    slug = models.CharField('栏目网址', max_length=256, db_index=True)
+    intro = models.TextField('栏目简介', default='')
+    home_display = models.BooleanField('首页显示', default=False)
+
+    def get_absolute_url(self):
+        return reverse('wiki:column', args=(self.slug,))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '栏目'
+        verbose_name_plural = '栏目'
+        ordering = ['name']  # 按照哪个栏目排序
+
+
+class Article(models.Model):
+    column = models.ForeignKey(Column, default='', null=False, verbose_name='归属栏目')
+    author = models.ForeignKey('auth.User', blank=True, null=True, verbose_name='作者')
+
+    title = models.CharField('标题', max_length=256)
+    content = models.TextField()
+
+    pub_date = models.DateTimeField('发表时间', auto_now_add=True, editable=True)
+    update_time = models.DateTimeField('更新时间', auto_now=True, null=True)
+    published = models.BooleanField('正式发布', default=True)
+
+    def get_absolute_url(self):
+        return reverse('wiki:article', args=(self.column.slug, self.pk))
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = '文章'
+
+
+class IMG(models.Model):
+    name = models.CharField(max_length=255)
+    img = models.ImageField(upload_to='upload')
