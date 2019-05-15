760017145a23f7b038327ae7d59a59d8a9210f4b
RyQcan <13723926068@163.com>
2019-05-15 22:22:18 +0800

    view

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Column, Article, IMG
+
+
+class ColumnAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'intro', 'home_display')
+
+class IMGAdmin(admin.ModelAdmin):
+    list_display = ('name', 'img')
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'column', 'author', 'pub_date', 'update_time')
+    list_filter = ('column', 'author', 'pub_date')
+
+admin.site.register(Column, ColumnAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(IMG, IMGAdmin)
