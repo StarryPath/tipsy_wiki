6a3a207030ca52ec8805c6caf4b1c7f4df43d856
RyQcan <13723926068@163.com>
2019-05-15 20:27:15 +0800

    git

--- tipsy_wiki/urls.py
+++ tipsy_wiki/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin, auth
-from news import views as news_views
+from wiki import views as news_views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^$', news_views.index),
+    url(r'^$', wiki_views.index),
     url(r'^news/', include('news.urls', namespace='news')),
     # url('^', include('auth.urls', namespace='users')),
     url(r'^admin/', admin.site.urls),
