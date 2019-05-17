e6178b9eed1546d6df1dd1756d5ae8b893e0d71f
RyQcan <13723926068@163.com>
2019-05-17 19:28:22 +0800

    admin

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Column, Article, IMG, Team,NewUser
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 # from django.contrib.auth.models import User
 
 class ColumnAdmin(admin.ModelAdmin):
@@ -21,15 +21,10 @@ class ArticleAdmin(admin.ModelAdmin):
 class NewUserAdmin(admin.ModelAdmin):
     pass
 admin.site.register(NewUser, NewUserAdmin)
-# class ProfileInline(admin.StackedInline):
-#     model = NewUser
-#     verbose_name_plural = "profile"
 
-# class UserAdmin(BaseUserAdmin):
-#     inlines = (ProfileInline,)
 
 # admin.site.unregister(User)
-# admin.site.register(NewUser, BaseUserAdmin)
+# admin.site.register(NewUser, UserAdmin)
 admin.site.register(Column, ColumnAdmin)
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Team, TeamAdmin)
