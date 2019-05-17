d50f48868e8d7ee445ca8a2ba801e029cc9b13aa
RyQcan <13723926068@163.com>
2019-05-17 14:26:08 +0800

    create user and column

---
+++ creatdata.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from wiki.models import Column
+
+namelist = ['lintianxiang', 'jiaozhengang', 'liyanzhe', 'zhangdachuan',
+            'fuyao', 'jinzhen', 'lihuaxin', 'lisirui', 'linjinxiu', 'huangxin',
+            'wangxiaowei', 'wangyang', 'yangjiageng', 'zhanghanwen', 'zhangzundong',
+            'leipengqun', 'liujiahao', 'wangxuwu']
+
+for name in namelist:
+    user = User.objects.create_user(name, '123@123.com', name)
+
+
+columnlist=['CTF','WEB','PWN','REVERSE','WIKI']
+
+
+for c_name in columnlist:
+    Column = Column.objects.create(name=c_name, content=c_name )
