3f3cc6ce2da74c6041dd8c7e95b22bee1ce23bb3
RyQcan <13723926068@163.com>
2019-05-17 19:21:40 +0800

    a

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Column, Article, IMG, NewUser
+from .models import Column, Article, IMG, Team,NewUser
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 class ColumnAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'intro', 'home_display')
@@ -9,23 +9,28 @@ class ColumnAdmin(admin.ModelAdmin):
 class IMGAdmin(admin.ModelAdmin):
     list_display = ('name', 'img')
 
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'column', 'author', 'pub_date', 'update_time')
     list_filter = ('column', 'author', 'pub_date')
 
 
+class NewUserAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(NewUser, NewUserAdmin)
+# class ProfileInline(admin.StackedInline):
+#     model = NewUser
+#     verbose_name_plural = "profile"
 
-class ProfileInline(admin.StackedInline):
-    model = NewUser
-    can_delete = False
-    verbose_name_plural = "profile"
+# class UserAdmin(BaseUserAdmin):
+#     inlines = (ProfileInline,)
 
-class UserAdmin(BaseUserAdmin):
-    inlines = (ProfileInline,)
-
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
-
+# admin.site.unregister(User)
+# admin.site.register(NewUser, BaseUserAdmin)
 admin.site.register(Column, ColumnAdmin)
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Team, TeamAdmin)
 admin.site.register(IMG, IMGAdmin)
