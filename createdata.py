97a7d3629c5165deca01c121259573efd3c0a08f
RyQcan <13723926068@163.com>
2019-05-17 14:36:33 +0800

    fix

--- createdata.py
+++ createdata.py
@@ -14,4 +14,4 @@ columnlist=['CTF','WEB','PWN','REVERSE','WIKI']
 
 
 for c_name in columnlist:
-    Column = Column.objects.get_or_create(name=c_name, slug=c_name )
+    Column = Column.objects.create(name=c_name, slug=c_name )
