0e8703f0a391f1b01bb5cfc8032a4e6a2f7b27df
StarryPath <617741157@qq.com>
2019-05-18 16:12:52 +0800

    pep8

--- createdata.py
+++ createdata.py
@@ -3,34 +3,34 @@ from django.contrib.auth import get_user_model
 from wiki.models import Column
 from wiki.models import Team
 
-i=2
+i = 2
 User = get_user_model()
-User.objects.create_superuser(username='admin',email='QQ@qq.com',password='admin')
+User.objects.create_superuser(username='admin', email='QQ@qq.com', password='admin')
 User.objects.filter(id=1).update(team='攻防技术组')
-#添加用户
+# 添加用户
 namelist1 = ['lintianxiang', 'liujiahao', 'liyanzhe', 'zhangdachuan',
-            'fuyao', 'jinzhen','zhangruiqi']
+             'fuyao', 'jinzhen', 'zhangruiqi']
 
 for name in namelist1:
-   User.objects.create_user(name, '123@123.com', name)
-   User.objects.filter(id=i).update(team='攻防技术组')
-   i=i+1
+    User.objects.create_user(name, '123@123.com', name)
+    User.objects.filter(id=i).update(team='攻防技术组')
+    i = i + 1
 
 namelist2 = ['lihuaxin', 'lisirui', 'linjinxiu', 'huangxin',
-            'wangxiaowei', 'wangyang']
+             'wangxiaowei', 'wangyang']
 
 for name in namelist2:
     User.objects.create_user(name, '123@123.com', name)
     User.objects.filter(id=i).update(team='安全开发组')
-    i=i+1
+    i = i + 1
 
 namelist3 = ['yangjiageng', 'zhanghanwen', 'zhangzundong',
-            'leipengqun', 'jiaozhengang', 'wangxuwu']
+             'leipengqun', 'jiaozhengang', 'wangxuwu']
 
 for name in namelist3:
     User.objects.create_user(name, '123@123.com', name)
     User.objects.filter(id=i).update(team='数据挖掘组')
-    i=i+1
+    i = i + 1
 
 
 # 添加栏目
